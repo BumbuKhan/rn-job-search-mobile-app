0452e9c1e4127924461366fd6e5fb93e09cb3654
Gurban Gurbanov <qurban.qurbanov93@gmail.com>
2018-04-26 21:38:12 +0400

    Added google map

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,20 +1,21 @@
 import React, {Component} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
+import { MapView } from 'expo';
 
 export default class AuthScreen extends Component {
     render() {
         return (
-            <View style={styles.container}>
-                <Text>MapScreen</Text>
+            <View style={{ flex: 1 }}>
+                <MapView
+                    style={{ flex: 1 }}
+                    initialRegion={{
+                        latitude: 37.78825,
+                        longitude: -122.4324,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421,
+                    }}
+                />
             </View>
         );
     }
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-});
